21d01fcc7e308703ae00edca7dbb08be0904b3a7
jared edwards <jredwards519@gmail.com>
2017-06-18 09:08:47 -0400

    fixed templating issues and progressed toward more accurate layout

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const parser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = 8080;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
+
 app.use( parser.json() );
 app.use(parser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(port, _ => {
