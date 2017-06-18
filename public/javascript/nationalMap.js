83e573ea75ff303e5e3b1d02f050648f6d372d76
jared edwards <jredwards519@gmail.com>
2017-06-18 11:24:06 -0400

    updated notes, fixed nationalMap file

--- public/javascript/nationalMap.js
+++ public/javascript/nationalMap.js
@@ -1,62 +1,51 @@
 $(function () {
 
-    // General gauge options
-    var gaugeOptions = {
-        chart: { type: 'solidgauge' },
-        title: null,
-        pane: {
-          center: ["50%", "50%"],
-          size: "100%",
-          startAngle: 0,
-          endAngle: 360,
-          background: {
-            backgroundColor: "#fff",
-            innerRadius: "74%",
-            outerRadius: "100%",
-            shape: "arc",
-            borderColor: "#fff"
-          }
-        },
-        tooltip: { enabled: false },
-        yAxis: {
-          stops: [ [.1, "#e55745"] ],
-          minorTickInterval: null,
-          tickWidth: 0,
-          title: { enabled: false },
-          labels: { enabled: false },
-          min: 0,
-          max: 100,
-          gridLineColor: "#fff",
-          minorGridLineColor: "#fff",
-          minorTickColor: "#fff",
-          tickColor: "#fff"
-        },
-        credits: { enabled: false },
-        plotOptions: {
-          solidgauge: {
-            innerRadius: "74%",
-            dataLabels: {
-              enabled: false,
-              borderWidth: 0,
-              borderColor: "rgba(0,0,0,0)",
-              useHTML: !0
-            }
-          }
-      }
-    };
+    // Initiate the chart
+    $('#mapContainer').highcharts('Map', {
 
-    // This gauge
-    $('#gauge').highcharts(Highcharts.merge(gaugeOptions, {
+        title : {
+            text : 'Choose a state to learn more'
+        },
+
+        subtitle : {
+            text : ''
+        },
+
+        mapNavigation: {
+            enabled: false,
+        },
         
-        yAxis: {
-          min: 0,
-          max: 100
+        tooltip: {
+            enabled: false
         },
 
-        series: [{
-          data: [ 80 ] 
+        colorAxis: {
+                min: 0,
+                minColor: "#ffffff",
+                maxColor: "#ffffff",
+                labels: {
+                    format: "{value:.0f}%",
+                    style: {
+                        fontSize: "13px"
+                    }
+                }
+            },
+				credits: { enabled: false },
+        exporting: { enabled: false },
+        series : [{
+            data : [],
+            mapData: Highcharts.maps['countries/us/us-all'],
+            joinBy: 'hc-key',
+            name: '',
+            states: { },
+            dataLabels: { enabled: false }
+        }, {
+            name: 'Separators',
+            type: 'mapline',
+            data: Highcharts.geojson(Highcharts.maps['countries/us/us-all'], 'mapline'),
+            color: 'silver',
+            showInLegend: false,
+            enableMouseTracking: false
         }]
-
-    }));
-
+    });
 });
